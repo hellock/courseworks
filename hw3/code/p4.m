edf60fc28975088ba700b6f06fc43e37be0ac12e
Kai Chen <chenkaidev@gmail.com>
2016-03-31 19:23:30 +0800

    part of p4

--- hw3/code/p4.m
+++ hw3/code/p4.m
@@ -6,7 +6,21 @@ hold on;
 plot(neg, y, 'xk');
 set(gca, 'ytick', []);
 grid on;
-plot([3 3], [0 2], '-b');
-text(3.1, 1.3, '(a)');
+%%
+plot([3 3], [0 2], '-b', 'LineWidth', 2);
+xlabel('x');
+text(3.1, 1.3, '(1)');
 text(3.4, 1.2, '-');
 text(2.5, 1.2, '+');
+%%
+plot([8 8], [0 2], '-b', 'LineWidth', 2);
+xlabel('x');
+text(8.1, 1.3, '(2)');
+text(8.4, 1.2, '-');
+text(7.5, 1.2, '+');
+%%
+plot([8 8], [0 2], '-b', 'LineWidth', 2);
+xlabel('x');
+text(8.1, 1.3, '(3)');
+text(8.4, 1.2, '-');
+text(7.5, 1.2, '+');
