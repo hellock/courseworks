553476ffcbe47c05a02a9c14319648f3a9d9618d
Kai Chen <chenkaidev@gmail.com>
2016-04-07 13:00:42 +0800

    modify errors

--- hw3/code/p4.m
+++ hw3/code/p4.m
@@ -7,19 +7,19 @@ plot(neg, y, 'xk');
 set(gca, 'ytick', []);
 grid on;
 %%
-plot([3 3], [0 2], '-b', 'LineWidth', 2);
+plot([3 3], [0 2], '-b', 'LineWidth', 3);
 xlabel('x');
 text(3.1, 1.3, '(1)');
 text(3.4, 1.2, '-');
 text(2.5, 1.2, '+');
 %%
-plot([8 8], [0 2], '-b', 'LineWidth', 2);
+plot([8 8], [0 2], '-b', 'LineWidth', 3);
 xlabel('x');
 text(8.1, 1.3, '(2)');
 text(8.4, 1.2, '-');
 text(7.5, 1.2, '+');
 %%
-plot([8 8], [0 2], '-b', 'LineWidth', 2);
+plot([8 8], [0 2], '-b', 'LineWidth', 3);
 xlabel('x');
 text(8.1, 1.3, '(3)');
 text(8.4, 1.2, '-');
